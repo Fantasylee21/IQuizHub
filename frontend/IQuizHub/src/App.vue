2dd8ad38c2fb677b0f2c4594a6565e5107b61507
范兴堃 <12788358+fan-xingkun@user.noreply.gitee.com>
2024-07-15 22:00:34 +0800

    布局排版

--- frontend/IQuizHub/src/App.vue
+++ frontend/IQuizHub/src/App.vue
@@ -1,11 +1,51 @@
 <template>
-    <h1>哈哈哈</h1>
+    <div class="common-layout">
+        <el-container>
+            <el-header class="header">
+                <GlobalMenu/>
+            </el-header>
+            <el-container>
+                <el-aside :width="!isCollapse ? '64px' : '200px'">
+                    <GlobalSidebar @toggleCollapse="toggleCollapse"/>
+                </el-aside>
+                <el-main class="main-content">Main</el-main>
+            </el-container>
+        </el-container>
+    </div>
 </template>
 
 <script setup lang="ts">
+import {ref} from 'vue';
+import GlobalSidebar from "@/components/GlobalComponents/GlobalSidebar.vue";
+import GlobalMenu from "@/components/GlobalComponents/GlobalMenu.vue";
 
+const isCollapse = ref(false);
+
+const toggleCollapse = () => {
+    isCollapse.value = !isCollapse.value;
+};
 </script>
 
 <style scoped>
+.header {
+    border-bottom: 1px solid var(--el-menu-border-color);
+}
 
+.common-layout {
+    height: 100vh;
+    margin: 0 0 0 0 !important;
+}
+
+.el-container {
+    height: 100%;
+}
+
+.el-aside {
+    transition: width 0.3s;
+}
+
+.main-content {
+    flex: 1;
+    padding: 20px;
+}
 </style>
---
+++ frontend/IQuizHub/src/components/GlobalComponents/GlobalSidebar.vue
@@ -0,0 +1,89 @@
+<template>
+    <el-menu
+            default-active="2"
+            class="el-menu-vertical-demo"
+            :collapse="isCollapse"
+            @open="handleOpen"
+            @close="handleClose"
+    >
+        <el-menu-item @click="toggleCollapse" index="1">
+            <el-icon>
+                <component :is="isCollapse ? ArrowRight : ArrowLeft"/>
+            </el-icon>
+            <template #title>Toggle Collapse</template>
+        </el-menu-item>
+        <el-sub-menu index="2">
+            <template #title>
+                <el-icon>
+                    <location/>
+                </el-icon>
+                <span>Navigator One</span>
+            </template>
+            <el-menu-item-group>
+                <template #title><span>Group One</span></template>
+                <el-menu-item index="1-1">item one</el-menu-item>
+                <el-menu-item index="1-2">item two</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="Group Two">
+                <el-menu-item index="1-3">item three</el-menu-item>
+            </el-menu-item-group>
+            <el-sub-menu index="1-4">
+                <template #title><span>item four</span></template>
+                <el-menu-item index="1-4-1">item one</el-menu-item>
+            </el-sub-menu>
+        </el-sub-menu>
+        <el-menu-item index="3">
+            <el-icon>
+                <icon-menu/>
+            </el-icon>
+            <template #title>Navigator Two</template>
+        </el-menu-item>
+        <el-menu-item index="4" disabled>
+            <el-icon>
+                <document/>
+            </el-icon>
+            <template #title>Navigator Three</template>
+        </el-menu-item>
+        <el-menu-item index="5">
+            <el-icon>
+                <setting/>
+            </el-icon>
+            <template #title>Navigator Four</template>
+        </el-menu-item>
+    </el-menu>
+</template>
+
+<script lang="ts" setup>
+import {ref, defineEmits} from 'vue';
+import {
+    Document,
+    Menu as IconMenu,
+    Location,
+    Setting,
+} from '@element-plus/icons-vue';
+import {ArrowLeft, ArrowRight} from '@element-plus/icons-vue';
+
+const isCollapse = ref(true);
+const handleOpen = (key: string, keyPath: string[]) => {
+    console.log(key, keyPath);
+};
+const handleClose = (key: string, keyPath: string[]) => {
+    console.log(key, keyPath);
+};
+const emit = defineEmits(['toggleCollapse']);
+const toggleCollapse = () => {
+    isCollapse.value = !isCollapse.value;
+    emit('toggleCollapse');
+};
+</script>
+
+<style scoped>
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+    width: 200px;
+    min-height: 400px;
+}
+
+.el-menu-vertical-demo.el-menu--collapse {
+    width: 64px;
+}
+</style>
