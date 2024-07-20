30e66bc82d50731d8dc4e5a78d0b868893237a09
范兴堃 <12788358+fan-xingkun@user.noreply.gitee.com>
2024-07-20 20:45:02 +0800

    ai提问接口对接成功

--- frontend/IQuizHub/src/views/MyTest.vue
+++ frontend/IQuizHub/src/views/MyTest.vue
@@ -1,133 +1,34 @@
 <template>
-    <div class="editor-container">
-        <div class="editor-left">
-            <el-tiptap v-model:content="content" :extensions="extensions" ref="editorRef"/>
-        </div>
-        <div class="editor-right">
-            <el-button type="primary" @click="insertText(' (  ) ')">插入选择框</el-button>
-            <el-button type="primary" @click="insertText(' ____ ')">插入填空线</el-button>
-            <el-button type="primary" @click="addOption">添加选项</el-button>
-            <div class="option-input">
-                <div class="option-list">
-                    <el-input v-for="(option, index) in options" :key="index" v-model="option.text"
-                              placeholder="请输入选项内容" class="option-item">
-                        <template #append>
-                            <el-button type="danger" @click="removeOption(index)">删除</el-button>
-                        </template>
-                    </el-input>
-                </div>
-            </div>
-        </div>
+    <div style="width: 1200px; margin: 0 auto; padding-top: 25px;">
+        <h2>单选题展示</h2>
+        <SingleChoiceQuestion :question="question"/>
+        <SingleChoiceQuestion :question="question"/>
+        <SingleChoiceQuestion :question="question"/>
+
     </div>
 </template>
 
 <script setup lang="ts">
-import {ref, onMounted} from 'vue';
-import {
-    // necessary extensions
-    Doc,
-    Text,
-    Paragraph,
-    //________________________
-    Bold,
-    Underline,
-    Italic,
-    Strike,
-    BulletList,
-    OrderedList,
-    Link,
-    Image,
-    FontSize,
-    FontFamily,
-} from 'element-tiptap-vue3-fixed';
+import {ref} from 'vue';
+import SingleChoiceQuestion from '@/components/GlobalComponents/Questions/SingleChoice.vue';
 
-const extensions = [
-    Doc,
-    Text,
-    Paragraph,
-    Bold.configure({bubble: true}),
-    Underline.configure({bubble: true, menubar: false}),
-    Italic,
-    Strike,
-    BulletList,
-    OrderedList,
-    Link,
-    Image,
-    FontSize.configure({bubble: true}),
-    FontFamily.configure({bubble: true}),
-];
-
-const content = ref(`
-  <h1>Heading</h1>
-  <p>This Editor is awesome!</p>
-`);
-
-const editorRef = ref<InstanceType<typeof import('element-tiptap-vue3-fixed').ElTiptap> | null>(null);
-
-onMounted(() => {
-    if (editorRef.value) {
-        const editor = editorRef.value.editor;
-    }
+const question = ref({
+    pattern: "single_choice",
+    description: "<p>I <span style='text-decoration: underline;'>shall answer</span> your question after class. 划线部分的句子成分为:</p>",
+    id: 1,
+    choices: [
+        "<p>主语</p>",
+        "<p>宾语</p>",
+        "<p>谓语</p>",
+        "<p>定语</p>"
+    ],
+    storeId: 568252,
+    bigQuestionId: 568252
 });
-
-const insertText = (text: string) => {
-    if (editorRef.value) {
-        const editor = editorRef.value.editor;
-        editor.chain().focus().insertContent(text).run();
-    }
-};
-
-const options = ref<{ text: string }[]>([]);
-
-const addOption = () => {
-    options.value.push({text: ''});
-};
-
-const removeOption = (index: number) => {
-    options.value.splice(index, 1);
-};
 </script>
 
 <style scoped>
-.editor-container {
-    margin: 70px auto 0;
-    width: 1200px;
-    display: flex;
-}
-
-.editor-left {
-    width: 70%;
-}
-
-.editor-right {
-    width: 30%;
-    margin-left: 20px;
-}
-
-.option-input {
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
-    margin-top: 20px;
-}
-
-.add-option {
-    text-align: right;
-}
-
-.option-list {
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
-}
-
-.option-item {
-    display: flex;
-    align-items: center;
-    gap: 10px;
-}
-
-.option-item .el-input {
-    flex: 1;
+h2 {
+    margin-bottom: 20px;
 }
 </style>
