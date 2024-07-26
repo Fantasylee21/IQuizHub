ce64fbe69895063f954d49a77623fefff53ce2c3
范兴堃 <12788358+fan-xingkun@user.noreply.gitee.com>
2024-07-26 11:44:02 +0800

    题单详情界面

---
+++ frontend/IQuizHub/src/components/SheetDetail/SDList.vue
@@ -0,0 +1,98 @@
+<template>
+    <div class="table-container">
+        <el-table :data="tableData" style="width: 100%">
+            <el-table-column prop="id" label="编号">
+            </el-table-column>
+            <el-table-column prop="create_time" label="创建时间">
+            </el-table-column>
+            <el-table-column label="题目名称">
+                <template v-slot="scope">
+                    <el-link @click.prevent="navigateToDetail(scope.row.id)" type="primary" underline>{{
+                            scope.row.title
+                        }}
+                    </el-link>
+                </template>
+            </el-table-column>
+            <el-table-column label="题目作者">
+                <template v-slot="scope">
+                    <el-link :href="scope.row.url" type="info" underline>{{ scope.row.author }}</el-link>
+                </template>
+            </el-table-column>
+            <el-table-column label="题目类型">
+                <template v-slot="scope">
+                    <el-tag v-if="scope.row.type =='True/False' ">判断题</el-tag>
+                    <el-tag v-else-if="scope.row.type =='single_choice' ">单选题</el-tag>
+                    <el-tag v-else-if="scope.row.type =='multiple_choice' ">多选题</el-tag>
+                    <el-tag v-else-if="scope.row.type =='fill_blanks' ">填空题</el-tag>
+                </template>
+            </el-table-column>
+            <el-table-column label="操作">
+                <template v-slot="scope">
+                    <el-button type="danger" @click="deleteRow(scope.row.id)" size="small">删除</el-button>
+                </template>
+            </el-table-column>
+        </el-table>
+        <el-pagination
+                @current-change="pageChange"
+                :current-page="currentPage"
+                :page-size="pageSize"
+                :total="total"
+                layout="prev, pager, next">
+        </el-pagination>
+    </div>
+</template>
+
+<script setup lang="ts">
+import {defineProps, ref, defineEmits} from 'vue'
+import router from '@/router'
+
+const emit = defineEmits(['pageChange', 'deleteRow']);
+const props = defineProps({
+    tableData: Array,
+    total: Number
+});
+
+const currentPage = ref(1);
+const pageSize = ref(20);
+
+
+const pageChange = (pageNew: number) => {
+    emit('pageChange', pageNew);
+    currentPage.value = pageNew;
+};
+
+const navigateToDetail = (id: number) => {
+    router.push(`/question-detail/${id}`);
+};
+
+const deleteRow = (deleteId: number) => {
+    emit('deleteRow', deleteId);
+};
+
+
+</script>
+
+<style scoped>
+.table-container {
+    margin-top: 20px;
+    margin-bottom: 40px;
+    background: linear-gradient(to right, #e6f7ff, #d4e8ff);
+    padding: 20px;
+    border-radius: 8px;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+}
+
+.el-table th,
+.el-table td {
+    text-align: center;
+}
+
+.el-link {
+    font-weight: bold;
+}
+
+.el-pagination {
+    margin-top: 20px;
+    text-align: center;
+}
+</style>
--- frontend/IQuizHub/src/views/SheetDetail.vue
+++ frontend/IQuizHub/src/views/SheetDetail.vue
@@ -20,14 +20,88 @@
         </div>
     </div>
     <div class="sheet-detail-container">
-
+        <div class="sheet-detail-nav">
+            <el-tabs v-model="activeName" class="demo-tabs">
+                <el-tab-pane label="题单简介" name="first">
+                    <div class="sheet-description">
+                        <div class="description-left">
+                            <h2>题单简介</h2>
+                            题单简介。。。。。。。。。。。。。。。。。。。。。。。。
+                        </div>
+                        <div class="description-right">
+                            <el-card class="detail-card">
+                                <div class="sheet-id">
+                                    <p>题单编号</p>
+                                    <p style="padding-right: 15px">{{ questionId }}</p>
+                                </div>
+                                <div class="sheet-creator">
+                                    <p>创建者</p>
+                                    <el-popover
+                                            placement="top-start"
+                                            width="300"
+                                            :title="creator"
+                                            trigger="hover"
+                                    >
+                                        <template #reference>
+                                            <el-button>{{ creator }}</el-button>
+                                        </template>
+                                        <div class="creator_profile">
+                                            <div class="creator_avatar_description">
+                                                <el-avatar size="50" src="@/assets/avatar.jpg"
+                                                           style="float: left"/>
+                                                <p>这个家伙很懒，什么也没有留下 </p>
+                                            </div>
+                                            <div class="creator_details">
+                                                <p style="margin-bottom: 10px">
+                                                    关注 <span>0</span> 粉丝 <span>32k</span> 排名 <span>19k</span>
+                                                </p>
+                                                <el-button type="primary" size="small">关注</el-button>
+                                                <el-button size="small">私信</el-button>
+                                            </div>
+                                        </div>
+                                    </el-popover>
+                                </div>
+                                <div class="sheet-type">
+                                    <p>题单类型</p>
+                                    <el-tag type="info" style="width: 60px; height: 32px">{{ questionType }}</el-tag>
+                                </div>
+                            </el-card>
+                            <el-card class="detail-progress">
+                                <h3 style="margin-bottom: 20px">我通过的题目 5 / 15</h3>
+                                <el-progress
+                                        :percentage="33"
+                                        :stroke-width="15"
+                                        striped
+                                        striped-flow
+                                />
+                            </el-card>
+                        </div>
+                    </div>
+                </el-tab-pane>
+                <el-tab-pane label="题目列表" name="second">
+                    <div class="sheet-list">
+                        <SDList/>
+                    </div>
+                </el-tab-pane>
+            </el-tabs>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
+import {ref} from "vue";
+import SDList from "@/components/SheetDetail/SDList.vue"
+
 const collect = () => {
 
 }
+
+const activeName = ref('first')
+
+const questionId = ref(100)
+const creator = ref('洛谷')
+const questionType = ref('官方题单')
+
 </script>
 
 <style scoped>
@@ -72,5 +146,71 @@ h1 {
 .sheet-detail-container {
     width: 1200px;
     margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+}
+
+.demo-tabs > .el-tabs__content {
+    padding: 32px;
+    color: #6b778c;
+    font-size: 32px;
+    font-weight: 600;
+}
+
+.sheet-description {
+    display: flex;
+    width: 100%;
+    height: 100%;
+}
+
+.description-left {
+    width: 70%;
+    padding: 10px;
+}
+
+.description-right {
+    width: 30%;
+}
+
+h2 {
+    font-size: 20px;
+    margin-bottom: 2em;
+    font-weight: bold;
+    line-height: 1.2;
+}
+
+.detail-card {
+    padding: 10px 10px 0;
+    display: flex;
+    flex-direction: column;
+}
+
+.sheet-id, .sheet-creator, .sheet-type {
+    margin: 10px 0 25px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.creator_avatar_description {
+    display: flex;
+    flex-wrap: wrap;
+}
+
+.creator_avatar_description p {
+    margin-left: 10px;
+    flex: 1;
+}
+
+.creator_details {
+    clear: both;
+    margin-top: 10px;
+}
+
+.detail-progress {
+    margin-top: 10px;
+    padding-right: 10px;
 }
 </style>
+
+
